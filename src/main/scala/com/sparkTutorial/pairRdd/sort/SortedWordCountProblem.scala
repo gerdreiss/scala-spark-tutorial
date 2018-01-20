8e192506f7945d1d0ac60f23bd19c2c9335689a3
Gerd Reiss <gerdreiss@jscriptive.com>
2018-01-20 10:37:32 +0100

    sort by (key)

--- src/main/scala/com/sparkTutorial/pairRdd/sort/SortedWordCountProblem.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/sort/SortedWordCountProblem.scala
@@ -1,17 +1,40 @@
 package com.sparkTutorial.pairRdd.sort
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
 
-object SortedWordCountProblem {
 
-    /* Create a Spark program to read the an article from in/word_count.text,
-       output the number of occurrence of each word in descending order.
+object SortedWordCountProblem extends App {
 
-       Sample output:
+  /*
+     Create a Spark program to read the an article from in/word_count.text,
+     output the number of occurrence of each word in descending order.
 
-       apple : 200
-       shoes : 193
-       bag : 176
-       ...
-     */
+     Sample output:
+
+     apple : 200
+     shoes : 193
+     bag : 176
+     ...
+   */
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val conf = new SparkConf().setAppName("wordCounts").setMaster("local[*]")
+  val sc = new SparkContext(conf)
+
+  sc.textFile("in/word_count.text")
+    .flatMap(_.split("\\s+|\\.|,|\\(|\\)|\\[|\\]"))
+    .filter(_.nonEmpty)
+    .map((_, 1))
+    .reduceByKey(_ + _)
+    //.map(_.swap)
+    //.sortByKey(ascending = false)
+    //.map(_.swap)
+    .sortBy(_._2, ascending = false)
+    .collect
+    .foreach {
+      case (word, count) => println(s"$word : $count")
+    }
+
 }
 
